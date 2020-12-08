ff63b73f84953a3fc74d61a7a1202400145b36ca
DavidK1717 <dkirkman17@gmail.com>
2020-12-08 23:39:49 +0000

    make listingsource description unique

--- app/models.py
+++ app/models.py
@@ -56,7 +56,7 @@ def load_user(user_id):
 
 class ListingSource(db.Model): 
     id = db.Column(db.Integer, primary_key=True) 
-    description = db.Column(db.String(50))   
+    description = db.Column(db.String(50), unique=True)
 
     def __repr__(self):
         return self.id
