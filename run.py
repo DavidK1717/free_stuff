93cc55b1a3aa7d5cda4e84aac9d54a160187bf94
DavidK1717 <dkirkman17@gmail.com>
2020-12-08 19:05:09 +0000

    Remove register option

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ from app.models import User, ListingSource, Listing
 
 
 app = create_app()
+application = app
 
 if __name__ == '__main__':
      app.run(debug=True, use_debugger=False, use_reloader=False, passthrough_errors=True)
