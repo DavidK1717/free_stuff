e35615e45dae66a893c359de7cffe2e8619075a1
DavidK1717 <dkirkman17@gmail.com>
2020-12-08 19:32:08 +0000

    Remove other register link and exclude run.py

--- run.py
+++ run.py
@@ -5,9 +5,6 @@ from app.models import User, ListingSource, Listing
 app = create_app()
 application = app
 
-if __name__ == '__main__':
-     app.run(debug=True, use_debugger=False, use_reloader=False, passthrough_errors=True)
-
 
 @app.shell_context_processor
 def make_shell_context():
